c6eef73aec28a7dfdd180cfcbf868dda2a580368
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-06-08 23:59:44 +0100

    implemented the Icon component and added to ListItem Component

--- App.js
+++ App.js
@@ -8,11 +8,12 @@ import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
 import MessagesScreen from "./app/components/MessagesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 const App = () => {
   return (
     <Screen>
-      <Icon name="email" />
+      <ListItem title="musa" ImageComponent={<Icon name="email" />} />
     </Screen>
   );
 };
