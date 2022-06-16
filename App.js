d4153f2183660dacab25f55d3f14d37673cd27ff
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-06-16 23:50:08 +0100

    optimized the LoginScreen component & App.js

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-import React, { useState } from "react";
-import { Text } from "react-native";
-import Screen from "./app/components/Screen";
+import React from "react";
 
-import AppTextInput from "./app/components/AppTextInput";
 import LoginScreen from "./app/screens/LoginScreen";
-import ListItemsDeleteAction from "./app/components/ListItemsDeleteAction";
-import View from "./app/screens/ViewImageScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ListingScreen from "./app/screens/ListingScreen";
-import AccountScreen from "./app/screens/AccountScreen";
-import AppPicker from "./app/components/AppPicker";
-import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
-import MessagesScreen from "./app/components/MessagesScreen";
-import ListItem from "./app/components/ListItem";
-import PickerItem from "./app/components/PickerItem";
 
 const App = () => {
   return <LoginScreen />;
